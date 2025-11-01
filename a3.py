693b3777f46441285948fc1f03f7346c7a5a64cc
prateeksha-coder <prateeksha.codingal@gmail.com>
2025-11-01 20:17:57 +0530

    commit

--- a3.py
+++ a3.py
@@ -1,6 +1,6 @@
 # Program to count number of lines in this file
 # Opening a file
-file = open("Codingal.txt","r")
+file = open("Codingal2.txt","r")
 Counter = 0
 
 # Reading from file
@@ -8,6 +8,7 @@ Content = file.read()
 # splitting content into lines
 # and storing them in a list
 CoList = Content.split("\n")
+print(CoList)
 Counter=len(CoList)
 	
 print("This is the number of lines in the file")
